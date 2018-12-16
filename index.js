16701b09eba04e2921566bca230a843eacd6c6a2
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-12-16 22:09:48 +0100

    Defined Elevator class and basic methods

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 class Elevator {
-  constructor(currentFloor, minFloor, maxFloor) {
+  constructor(currentFloor, bottomFloor, topFloor) {
     this.currentFloor = currentFloor;
-    this.minFloor = minFloor;
-    this.maxFloor = maxFloor;
+    this.bottomFloor = bottomFloor;
+    this.topFloor = topFloor;
+  }
+
+  getFloor() {
+    return this.currentFloor;
+  }
+
+  canGoUp() {
+    return this.currentFloor < this.topFloor;
+  }
+
+  canGoDown() {
+    return this.currentFloor > this.bottomFloor;
   }
 
   goUp() {
-
+    if (this.canGoUp()) {
+      this.currentFloor += 1;
+      console.log(`On ${this.getFloor}, going up one level`);
+    } else {
+      console.log(`You are currently on ${this.topFloor}, the elevator cannot go higher`);
+    }
   }
 
   goDown() {
-
+    if (this.canGoDown()) {
+      this.currentFloor -= 1;
+      console.log(`On ${this.currentFloor}, going down one level`);
+    } else {
+      console.log(`You are currently on ${this.bottomFloor}, the elevator cannot go lower`);
+    }
   }
 }
+
+const elevator1 = new Elevator(0, -2, 4);
+const elevator2 = new Elevator(2, 0, 2);
